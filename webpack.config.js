19d5b7a54998cc1dff43e82d2eceb55decc7679b
Max Schram <maxbschram@gmail.com>
2015-10-09 21:56:19 -0700

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var path = require("path);
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/reactApp.js",
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  }
+};
